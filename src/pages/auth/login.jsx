fccdfb7bfd061d08e72d9884fc6721c586eed2e5
Muhammad Anies Wahdie <manies5396@gmail.com>
2025-10-08 16:34:13 +0700

    feat: setup antd

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,5 +1,12 @@
+import { Button } from "antd";
+
 const Login = () => {
-  return <div className="text-black bg-amber-300">Login Page</div>;
+  return (
+    <div className="text-black bg-amber-300">
+      <span>Login</span>
+      <Button type="primary">Login Button</Button>
+    </div>
+  );
 };
 
 export default Login;
